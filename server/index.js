cd1f41120e9cfdcf07db16e59e72316ed0d07c2a
FloricaValentin <floricavalentin@yahoo.com>
2024-07-09 01:53:55 +0300

    Update index.js
    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
-const  typeDefs = require ('./graphql/schema')
-const resolvers = require('./graphql/resolvers')
+const { typeDefs } = require('./graphql/schema');
+const { resolvers } = require('./graphql/resolvers');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.error(err));
+  .catch(err => console.error('MongoDB Connection Error:', err));
 
 const PORT = process.env.PORT || 4000;
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true,
+});
 
 server.listen({ port: PORT }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
